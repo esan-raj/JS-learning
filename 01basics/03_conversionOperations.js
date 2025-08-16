77770f9a12acdb82355cb0430890fde6eb2f4890
Esan Raj <112330540+esan-raj@users.noreply.github.com>
2025-08-16 15:37:05 +0000

    Conversion Operations

---
+++ 01basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+/* 
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+let isLoggedIn = "esan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "esan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
